ec9df5817a1e42e8faeb876622990d1359584412
MechaDragonX <theofficialrv@gmail.com>
2020-10-31 11:45:16 -0700

    Forgot to add new files in last commit

---
+++ Audio.cs
@@ -0,0 +1,45 @@
+﻿using ATL;
+using System.IO;
+
+namespace MetaSlam
+{
+    class Audio
+    {
+        /// <summary>
+        /// Name all audio files in a directory based on the file's metadata
+        /// </summary>
+        /// <param name="path">Path to File</param>
+        public static void NameDirectory(string path)
+        {
+            foreach(string file in Directory.EnumerateFiles(path))
+            {
+                if(Path.GetFileNameWithoutExtension(file) != "folder")
+                    NameFile(file);
+            }
+        }
+        /// <summary>
+        /// Name Audio File Based on File's Metadata
+        /// </summary>
+        /// <param name="path">Path to File</param>
+        private static void NameFile(string path)
+        {
+            string newPath = path.Substring(0, path.Length - Path.GetFileName(path).Length);
+            Track track = new Track(path);
+            string name = "";
+            string extension = Path.GetExtension(path);
+
+            if(track.TrackNumber < 10)
+                name += $"0{track.TrackNumber} - ";
+            else
+                name += $"{track.TrackNumber} - ";
+            name += track.Title;
+            if(track.Artist != track.AlbumArtist)
+                name += $" - {track.Artist}";
+            name = Utility.CheckFixFilename(name);
+            name += extension;
+            newPath += name;
+
+            File.Move(path, newPath);
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaSlam
+{
+    class Utility
+    {
+        private static readonly char[] illegalChar = { '<', '>', ':', '"', '/', '\\', '|', '?', '*' };
+
+        /// <summary>
+        /// Check the filename for illegal characters and the replace them with legal characters or remove them
+        /// </summary>
+        /// <param name="name">Filename Without Extension</param>
+        /// <returns></returns>
+        public static string CheckFixFilename(string name)
+        {
+            for(int i = 0; i < illegalChar.Length; i++)
+            {
+                if(name.Contains(illegalChar[i]))
+                {
+                    switch(illegalChar[i])
+                    {
+                        case '<':
+                        case '>':
+                        case '/':
+                        case '\\':
+                        case '|':
+                        case '*':
+                            name = name.Replace(illegalChar[i], '-');
+                            break;
+                        case '"':
+                            name = name.Replace(illegalChar[i], '\'');
+                            break;
+                        case '?':
+                            name = name.Remove(name.IndexOf(illegalChar[i]), 1);
+                            break;
+                    }
+                }
+            }
+            return name;
+        }
+    }
+}
